e25ccc260e9a727082a259a52ea7f20dffc62107
Felix_J <111042013+DevFelixJ@users.noreply.github.com>
2023-02-02 09:52:10 +0100

    Update RandomBridge.cs
    Actualización para restuarar el puente.

--- RandomBridge.cs
+++ RandomBridge.cs
@@ -5,16 +5,48 @@ using UnityEngine;
 public class RandomBridge : MonoBehaviour
 {
     public GameObject[] puentes;
+    public Material sueloPuente;
+    public GameObject puenteElegido;
+    public GameObject puenteActivado;
+    
+
     void Start()
     {
         ActivarPuenteRandom();
+        EncontrarPuente();
+
+
+    }
+    private void Update()
+    {
+            StartCoroutine(EncontrarPuente());
+
+        
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     public void ActivarPuenteRandom()
     {
         int n = Random.Range(0, puentes.Length);
-        puentes[n].SetActive(true);
+        puenteElegido = puentes[n];
+        puenteElegido.SetActive(true);
+
+    }
+    public void restaurarPuente()
+    {
+        Transform puenteCubos;
+        puenteCubos = this.GetComponent<Transform>();
+        Destroy(puenteActivado);
+        puenteActivado = Instantiate(puenteElegido) as GameObject;
+        puenteActivado.transform.SetParent(puenteCubos, false);
+        puenteActivado.name = puenteElegido.name;
+        puenteElegido = puenteActivado;
+    }
+
+    public IEnumerator EncontrarPuente()
+    {
+        yield return new WaitForSeconds(1);
+        puenteActivado = GameObject.FindGameObjectWithTag("puente");
 
     }
 }
