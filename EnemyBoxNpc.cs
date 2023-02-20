1a5d7e8bb3a673777c756cbd782579c2bded0cee
Felix_J <111042013+DevFelixJ@users.noreply.github.com>
2023-02-20 09:55:22 +0100

    Add files via upload

---
+++ EnemyBoxNpc.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoxNpc : EnemyBox
+{
+    // Start is called before the first frame update
+    Color cambioVerde = Color.green;
+    public GameObject enemigoBlackNpc;
+    public GameObject Cubo5;
+    public Material renderMaterial;
+    public RandomBridge texturaPuente;
+    public EnemyBoxPista enemyBoxPista;
+
+    private void Awake()
+    {
+        enemigoBlackNpc.SetActive(false);
+    }
+    void Start()
+    {
+        texturaPuente = FindObjectOfType<RandomBridge>();
+        enemyBoxPista = FindObjectOfType<EnemyBoxPista>();
+    }
+    public override void PlayerInteractua()
+    {
+        Invoke("CambioRojo", 0.2f);
+        Invoke("ActivoEnemy", 0.3f);
+        Invoke("RestaurarEnemigo", 4.5f);
+
+        //Debug.Log("Test enemigo aparece ok");
+        //Destroy(enemigoBlackNpc, 7)
+    }
+
+    void ActivoEnemy()
+    {
+        enemigoBlackNpc.SetActive(true);
+    }
+    void CambioRojo()
+    {
+        Cubo5.GetComponent<Renderer>().material = renderMaterial;
+    }
+    public void RestaurarEnemigo()
+    {
+        enemigoBlackNpc.SetActive(false);
+    }
+
+}
