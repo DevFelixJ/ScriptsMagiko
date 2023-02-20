5fd4df2e3da35048e6d508294b992647d7833b82
Felix_J <111042013+DevFelixJ@users.noreply.github.com>
2023-02-20 17:10:10 +0100

    Script del golpe del Enemigo.
    Poniendo Booleano el Morir.

--- Enemy1.cs
+++ Enemy1.cs
@@ -9,7 +9,8 @@ public class Enemy1 : MonoBehaviour
     {
         if (other.gameObject.tag.Equals("Player"))
         {
-            other.GetComponent<Animator>().SetTrigger("Morir");
+            other.GetComponent<Animator>().SetBool("Morir", true);
+            Debug.Log("Animacion Morir3");
             StartCoroutine(MuertePlayer());
         }
     }
@@ -17,5 +18,6 @@ public class Enemy1 : MonoBehaviour
     {
         yield return new WaitForSeconds(4);
         GameObject.FindGameObjectWithTag("Player").GetComponent<DeadPlayer>().vivo = false;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("Morir", false);
     }
 }
